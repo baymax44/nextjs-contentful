5a261871d59603732a5baca6acd52b46e65880a6
baymax44 <smartbay2021@outlook.com>
2020-04-01 17:11:05 -0400

    getting contents from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
-export default function Recipes() {
+import { createClient } from 'contentful'
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  })
+
+  const res = await client.getEntries({ content_type: 'recipe' })
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+}
+
+export default function Recipes({ recipes }) {
   return (
     <div className="recipe-list">
       Recipe List
