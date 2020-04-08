78ea92da59a44c5999bf22c18606c77dcd5d4bcc
baymax44 <smartbay2021@outlook.com>
2020-04-07 21:05:53 -0400

    generating paths

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,36 @@
-export default function RecipeDetails() {
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: { recipe: items[0] }
+  }
+}
+
+export default function RecipeDetails({ recipe }) {
   return (
     <div>
       Recipe Details
