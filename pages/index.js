7ec46c1626f15b40625f8e3b00c131ed141c361b
baymax44 <smartbay2021@outlook.com>
2020-04-09 21:59:26 -0400

    incremental static regeneration

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,8 @@ export async function getStaticProps() {
   const res = await client.getEntries({ content_type: 'recipe' })
 
   return {
-    props: {
-      recipes: res.items
-    }
+    props: { recipes: res.items },
+    revalidate: 1
   }
 }
 
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -28,7 +28,8 @@ export async function getStaticProps({ params }) {
   })
 
   return {
-    props: { recipe: items[0] }
+    props: { recipe: items[0] },
+    revalidate: 1
   }
 }
 
